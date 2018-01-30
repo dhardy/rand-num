69e078f2258921ec499d0b90acca284b044044ef
Diggory Hardy <git@dhardy.name>
2018-01-30 12:03:43 +0000

    Use thread_rng

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate rand;
 use rand::*;
 
 fn main() {
-    let mut rng = OsRng::new().unwrap();
+    let mut rng = thread_rng();
     
     println!("Random u32: 0x{0:x} = {0}", rng.next_u32());
     println!("Random u64: 0x{0:x} = {0}", rng.next_u64());
