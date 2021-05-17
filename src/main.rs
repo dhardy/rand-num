18d78e7c4b001a857147ec2283f22f25c61611e7
Diggory Hardy <git@dhardy.name>
2021-05-17 10:58:03 +0100

    Output all basic integer types

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@ use rand::prelude::*;
 
 fn main() {
     let mut rng = thread_rng();
+
+    macro_rules! out {
+        ($t:ty) => {
+            println!("Random {0}: 0x{1:x} = {1}", stringify!($t), rng.gen::<$t>());
+        };
+        ($t:ty, $($tt:ty),*) => {
+            out!($t);
+            out!($($tt),*);
+        };
+    }
     
-    println!("Random u32: 0x{0:x} = {0}", rng.next_u32());
-    println!("Random u64: 0x{0:x} = {0}", rng.next_u64());
+    out!(u8, u16, u32, u64, u128);
+    out!(i8, i16, i32, i64, i128);
 }
